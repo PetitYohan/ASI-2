dca984fb2e360881dddd78fd067d4e1e7ad26718
ClubSaumon07 <loic.ameglio@protonmail.com>
2022-11-14 17:16:47 +0100

    fix cardDto

--- asi2-backend-market/common/src/main/java/com/asi2/backendmarket/dto/card/CardDto.java
+++ asi2-backend-market/common/src/main/java/com/asi2/backendmarket/dto/card/CardDto.java
@@ -1,75 +1,72 @@
 package com.asi2.backendmarket.dto.card;
 
-public class CardDto {
+public class CardDto extends CardBasics {
+	private Integer id;
+	private float energy;
+	private float hp;
+	private float defence;
+	private float attack;
+	private float price;
+	private Integer userId;
 
-	public class CardDTO extends CardBasics {
-		private Integer id;
-		private float energy;
-		private float hp;
-		private float defence;
-		private float attack;
-		private float price;
-		private Integer userId;
-
-		public CardDTO() {
-
-		}
-
-		public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public float getEnergy() {
-			return energy;
-		}
-
-		public void setEnergy(float energy) {
-			this.energy = energy;
-		}
-
-		public float getHp() {
-			return hp;
-		}
-
-		public void setHp(float hp) {
-			this.hp = hp;
-		}
-
-		public float getDefence() {
-			return defence;
-		}
-
-		public void setDefence(float defence) {
-			this.defence = defence;
-		}
-
-		public float getAttack() {
-			return attack;
-		}
-
-		public void setAttack(float attack) {
-			this.attack = attack;
-		}
-
-		public float getPrice() {
-			return price;
-		}
-
-		public void setPrice(float price) {
-			this.price = price;
-		}
-
-		public Integer getUserId() {
-			return userId;
-		}
-
-		public void setUserId(Integer userId) {
-			this.userId = userId;
-		}
+	public CardDto() {
 
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public float getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(float energy) {
+		this.energy = energy;
+	}
+
+	public float getHp() {
+		return hp;
+	}
+
+	public void setHp(float hp) {
+		this.hp = hp;
+	}
+
+	public float getDefence() {
+		return defence;
+	}
+
+	public void setDefence(float defence) {
+		this.defence = defence;
+	}
+
+	public float getAttack() {
+		return attack;
+	}
+
+	public void setAttack(float attack) {
+		this.attack = attack;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
 }
