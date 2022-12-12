442cd49885f751cf54e4645ec4cf7b547c346a08
PetitYohan <yohan.petit@cpe.fr>
2022-12-12 21:02:21 +0100

    ajout selection cartes

--- achat-revente-cartes/src/components/game/Game.jsx
+++ achat-revente-cartes/src/components/game/Game.jsx
@@ -2,7 +2,7 @@ import NavBar from "../NavBar/NavBar";
 import Chat from "../Chat/Chat";
 import "./Game.css";
 import Button from "@mui/material/Button";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, selectCards } from "../../core/selectors";
 import { setCards } from "../../core/actions";
@@ -13,68 +13,78 @@ const title = "Game";
 const Game = () => {
   const dispatch = useDispatch();
   const socket = useContext(SocketContext);
-  const [isConnected, setIsConnected] = useState(socket.connected);
   const userCards = useSelector(selectCards);
   const userSelect = useSelector(selectUser);
+  const cardsList = [];
 
   useEffect(() => {
-    socket.on("co", () => {
-      console.log("co");
-      setIsConnected(true);
-    });
-
-    socket.on("deco", () => {
-      setIsConnected(false);
-    });
-
     const getUserCards = async () => {
-      const resp = await fetch("http://127.0.0.1/api/card/user/"+userSelect.idUser);
+      const resp = await fetch(
+        "http://127.0.0.1/api/card/user/" + userSelect.idUser
+      );
       const cards = await resp.json();
       dispatch(setCards(cards));
     };
 
     getUserCards();
-
-    return () => {
-      socket.off("co");
-      socket.off("deco");
-    };
   }, []);
 
-
-  const sendConnect = () => {
-    socket.emit("connectio");
-  };
-
-  const sendDeconnect = () => {
-    socket.emit("deconnectio");
-  };
-
   const sendJoinRoom = () => {
-    const userCardsSelected = [{ cardId: 12},{ cardId: 13 }];
-    socket.emit("joinRoom", userCardsSelected);
+    if (cardsList.length == 5) {
+      const userCardsSelected = cardsList;
+      socket.emit("joinRoom", userCardsSelected);
+    } else {
+      alert("Il faut selectionner 5 cartes pas plus pas moins !");
+    }
   };
 
   const sendDisconnectRoom = () => {
     socket.emit("disconnectRoom");
   };
 
+  function playWithThisCard(card) {
+    if (cardsList.indexOf(card) <= -1) {
+      document.getElementById(card.id).style.border = "0.2em solid green";
+      cardsList.push(card);
+    } else {
+      document.getElementById(card.id).style.border =
+        "0.2em solid rgb(44, 44, 44)";
+      const index = cardsList.indexOf(card);
+      if (index > -1) {
+        cardsList.splice(index, 1);
+      }
+    }
+  }
+
   return (
     <>
       <NavBar title={title} />
       <Chat />
-      <p>Connected: {"" + isConnected}</p>
-      <button onClick={sendConnect}>Send Connect</button>
-      <button onClick={sendDeconnect}>Send Deconnect</button>
       <button onClick={sendJoinRoom}>Send joinRoom</button>
       <button onClick={sendDisconnectRoom}>Send disconnectRoom</button>
-      <select>
+      <section>
         {userCards.map((card) => {
-          return(
-          <option value={card.cardId}>{card.name}</option>
-          )
+          return (
+            <div
+              class="cardToSelect"
+              id={card.id}
+              onClick={() => {
+                playWithThisCard(card);
+              }}
+            >
+              <div id="first_card">
+                {card.energy}⚡ {card.name} {card.hp}❤️
+              </div>
+              <img
+                id="img_cardGame"
+                src={card.smallImgUrl}
+                alt="Image de la carte"
+              ></img>
+              <div id="desc_card">{card.description}</div>
+            </div>
+          );
         })}
-      </select>
+      </section>
       <Button
         variant="outlined"
         onClick={() => {
